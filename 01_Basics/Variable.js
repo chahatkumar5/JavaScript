452510548aa5d356e6fd27e61f381b129ca5c37a
chahatkumar5 <97453388+chahatkumar5@users.noreply.github.com>
2025-06-06 11:49:26 +0000

    Variable module configured

---
+++ 01_Basics/Variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "chahat@gmail.com"
+var accountPassword = "qwerty"
+
+
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 132444 not allowed
+//never use var
+
+console.log(accountCity);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
