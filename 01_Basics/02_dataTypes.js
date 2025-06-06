5db7d6836f9d1303e15ec2dc0ee3614f5fccafe3
chahatkumar5 <97453388+chahatkumar5@users.noreply.github.com>
2025-06-06 12:14:50 +0000

    dataTypes Module Saved

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3+3);
+
+let name = "Suraj"
+let age = 23
+let isLoggedIn = false
+
+//number =>2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null => standalone value which is empty, it is an object
+//undefined => datatype/value not assigned, it can be a datatype
+//symbol => unique
+
+//object
+
+console.log(typeof "Chahat")
+
+console.log(typeof undefined);
