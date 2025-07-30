c668c2e5a374923483f5440c68a222553ebab5ec
chahatkumar5 <97453388+chahatkumar5@users.noreply.github.com>
2025-07-30 10:49:43 +0000

    Changes Staged

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -24,5 +24,58 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//______________________________________________________Operations______________________________________________________
+
+//console.log(1 + 2 + "3")
+let x=5;
+let y = ++x;
+
+//console.log(x, y); Expected output = 6, 6
+
+// let a=5;
+// let b= a++;
+//console.log(a, b); Expected output = 6, 5
+
+//console.log(1 + 2 + 5 + "6" + "7" + 6 + 5)
+
+
+//***************************************************ChatGPT Task**************************************************
+//Task 1 
+
+// let a = "10";
+// let b = "20";
+
+// a = Number(a);
+// b = Number(b);
+// console.log(a+b);
+
+//Task 2
+// let values = [0, 1, "", "hello", null, undefined, NaN, "false"];
+// let booleanValues = values.map(item => Boolean(item))
+// console.log(booleanValues)
+
+// let trueBooleanValues = booleanValues.filter(item => item === false)
+// console.log(trueBooleanValues)
+
+//Task 3
+
+// let input = '123.45px'
+// numberInput = Number(input)
+// console.log(numberInput) //output - NaN
+// console.log(typeof numberInput)
+
+//Task 4
+// let isOnline = true;
+// let isOffline = false;
+// console.log(Number(isOnline));//1
+// console.log(Number(isOffline));//0
+
+//Task 5  Loose vs Strict Equality
+let now = 5;
+let later = '5';
+console.log(now == later);//check only value not types
+console.log(now === later);// checks both value and dataType
